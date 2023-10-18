134f8ecfa44dc5c7aed25d11cef0b8954620fa15
matthew-ladipo <matthewladipo@gmail.com>
2023-10-18 15:31:50 +0100

    commit on popup backdrop

--- src/layouts/popUp/step5.jsx
+++ src/layouts/popUp/step5.jsx
@@ -1,19 +1,17 @@
 import classes from "./step5.module.css";
-import img1 from '../../image/icon-thank-you.svg';
-
+import img1 from "../../image/icon-thank-you.svg";
 
 const Step5 = (props) => {
   function closeBackdrop() {
     props.onCancel();
   }
-  
+
   return (
-    <div >
-     
+    <div>
       <div className={classes.final}>
         <div className={classes.pop}>
-      <img src={img1} alt="svg5" className={classes.img1} />
-      </div>
+          <img src={img1} alt="svg5" className={classes.img1} />
+        </div>
         <div className={classes.content}>
           <h1>Thank You</h1>
           <p>
@@ -23,7 +21,9 @@ const Step5 = (props) => {
           </p>
         </div>
         <br />
-        <button onClick={closeBackdrop}><a href="http://localhost:3000">Ok</a></button>
+        <button onClick={closeBackdrop}>
+          <a href="http://localhost:3000">Ok</a>
+        </button>
       </div>
     </div>
   );
